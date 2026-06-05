c05d3fcbf20d8a3aecde25cd9c2e8ccf29f6d909
hisoka <hisoka>
2026-06-05 10:53:40 +0100

    set up navbar components

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,11 @@
-import { useAuth } from "@clerk/react";
+import { SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
 import { Link } from "react-router";
-import { ShoppingBagIcon } from "lucide-react";
+import {
+  PlusCircleIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "lucide-react";
+import ThemeSelector from "./ThemeSelector";
 
 const Navbar = () => {
   const { isSignedIn } = useAuth();
@@ -9,9 +14,35 @@ const Navbar = () => {
       <div className="max-w-5xl mx-auto w-full px-4 flex justify-between items-center">
         <div className="flex">
           <Link to="/" className="btn btn-ghost gap-2">
-            <ShoppingBagIcon className="size-5 text-primary"/>
+            <ShoppingBagIcon className="size-5 text-primary" />
             <span className="text-lg font-bold">ProductStore</span>
           </Link>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <ThemeSelector />
+          {isSignedIn ? (
+            <>
+              <Link to="/create" className="btn btn-primary btn-sm gap-1">
+                <PlusCircleIcon className="size-5" />
+                <span className="hidden sm:inline">New Product</span>
+              </Link>
+              <Link to="/profile" className="btn btn-ghost btn-sm gap-1">
+                <UserIcon className="size-5" />
+                <span className="hidden sm:inline">Profile</span>
+              </Link>
+              <UserButton />
+            </>
+          ) : (
+            <>
+              <SignInButton mode="modal">
+                <button className="btn btn-ghost btn-sm">Sign In</button>
+              </SignInButton>
+              <SignUpButton mode="modal">
+                <button className="btn btn-primary btn-sm">Get Started</button>
+              </SignUpButton>
+            </>
+          )}
         </div>
       </div>
     </div>
