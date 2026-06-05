2a0bae2f237344f94dbfd5cd8bd4efed7d78ba74
hisoka <hisoka>
2026-06-05 15:45:37 +0100

    Set up axios and api and sync user with database

--- backend/src/routes/commentRoutes.ts
+++ backend/src/routes/commentRoutes.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import { requireAuth } from "@clerk/express";
-import * as commentController from "../controllers/commentController"
+import * as commentController from "../controllers/commentController";
 
 const commentRouter = Router();
 
 // POST /api/comments/:productId - Add comment to product (protected)
-commentRouter.post("/:productId", requireAuth(), commentController.createComment)
+commentRouter.post(
+  "/:productId",
+  requireAuth(),
+  commentController.createComment,
+);
 
 // DELETE /api/comments/:productId - Delete comment (protected + owner only)
-commentRouter.post("/:commentId", requireAuth(), commentController.deleteComment)
+commentRouter.delete(
+  "/:commentId",
+  requireAuth(),
+  commentController.deleteComment,
+);
 
 export default commentRouter;
